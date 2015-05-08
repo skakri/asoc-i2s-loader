6de32aece4216fc931aa31d9e1005099f438ec06
Kristaps Karlsons <kristaps.karlsons@gmail.com>
2015-05-08 15:49:30 +0000

    ASoC loader.
    Original work: Plugh Plover.

---
+++ asoc-i2s-loader.c
@@ -0,0 +1,86 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kmod.h>
+#include <linux/platform_device.h>
+#include <sound/simple_card.h>
+/*
+    inspired by https://github.com/msperl/spi-config
+    with thanks for https://github.com/notro/rpi-source/wiki
+    as well as Florian Meier for the rpi i2s and dma drivers
+
+    to use a differant (simple-card compatible) codec
+    change the codec name string in two places and the
+    codec_dai name string. (see codec's source file)
+
+    fmt flags are set for vanilla i2s with rpi as clock master
+
+    N.B. playback vs capture is determined by the codec choice
+*/
+
+void device_release_callback(struct device *dev) { /* do nothing */ };
+
+static struct asoc_simple_card_info snd_rpi_simple_card_info = {
+    .card = "snd_rpi_simple_card", // -> snd_soc_card.name
+    .name = "simple-card_codec_link", // -> snd_soc_dai_link.name
+    .codec = "dmic-codec", // -> snd_soc_dai_link.codec_name
+    .platform = "bcm2708-i2s.0", // -> snd_soc_dai_link.platform_name
+    .daifmt = 0,
+    .cpu_dai = {
+        .name = "bcm2708-i2s.0", // -> snd_soc_dai_link.cpu_dai_name
+        .fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS,
+        .sysclk = 0 },
+    .codec_dai = {
+        .name = "dmic-hifi", // -> snd_soc_dai_link.codec_dai_name
+        .fmt = 0,
+        .sysclk = 0 },
+};
+static struct platform_device snd_rpi_simple_card_device = {
+    .name = "asoc-simple-card", //module alias
+    .id = 0,
+    .num_resources = 0,
+    .dev = { .release = &device_release_callback,
+        .platform_data = &snd_rpi_simple_card_info, // *HACK ALERT*
+        },
+};
+static struct platform_device snd_rpi_codec_device = {
+        .name = "dmic-codec", //module alias
+        .id = -1,
+        .num_resources = 0,
+    .dev = { .release = &device_release_callback,
+        },
+};
+
+int hello_init(void)
+{
+    const char *dmaengine = "bcm2708-dmaengine"; //module name
+    int ret;
+
+    ret = request_module(dmaengine);
+    if (ret)
+        pr_alert("Unable to request module load '%s': %d\n",
+            dmaengine, ret);
+
+    ret = platform_device_register(&snd_rpi_codec_device);
+    if (ret)
+        pr_alert("Unable to register platform device '%s': %d\n",
+             snd_rpi_codec_device.name, ret);
+
+    ret = platform_device_register(&snd_rpi_simple_card_device);
+    if (ret)
+        pr_alert("Unable to register platform device '%s': %d\n",
+             snd_rpi_simple_card_device.name, ret);
+
+    pr_alert("ASoC simple-card I2S loader initialized\n");
+    return 0;
+}
+void hello_exit(void)
+{// you'll have to sudo modprobe -r the card & codec drivers manually (first?)
+    platform_device_unregister(&snd_rpi_simple_card_device);
+    platform_device_unregister(&snd_rpi_codec_device);
+    pr_alert("Goodbye World!\n");
+}
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_DESCRIPTION("ASoC simple-card I2S setup");
+MODULE_AUTHOR("Plugh Plover");
+MODULE_LICENSE("GPL v2");
